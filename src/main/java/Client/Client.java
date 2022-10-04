a7f1623bdb77033dde9700438cc651d140857a44
unknown <otmax.vft@gmail.com>
2022-10-04 22:40:54 +0100

    client is ready

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -2,9 +2,12 @@
 package Client;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -22,23 +25,62 @@ public class Client {
         String host = ClientConfig.HOST;
 
         try {
-            Socket socket = new Socket(host, port);
+            Socket clientSocket = new Socket(host, port);
+            
+            BufferedReader bufferedInputStreamReader = null;
+            BufferedWriter bufferedOutputStreamWriter = null;
+            
                   
-            //InputStreamReader reader = new InputStreamReader(socket.getInputStream()); 
+            // 1)- Creating socket streams
             
-            /*BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+            InputStream socketInput = clientSocket.getInputStream();//get the input streams from the server
+            OutputStream socketOutput = clientSocket.getOutputStream();//create the input stream to send to the server
             
-            String message = input.readLine();
             
-            PrintWriter outMsg = new PrintWriter(socket.getOutputStream());
+            // 2)- Reading from & Writing to : the socket by the previously created streams
             
-            outMsg.println(message);
-            outMsg.flush();*/
+            InputStreamReader socketInputReader = new InputStreamReader(socketInput); // read the socket streams 
+            OutputStreamWriter socketOutputWriter = new OutputStreamWriter(socketOutput);// write output streams
             
-            socket.close();
+            
+            // 3)- Append the socket io stream readers to buffered reader form more effeciency
+            
+            bufferedInputStreamReader = new BufferedReader(socketInputReader);//buffered read the socket streams for effeciency
+            bufferedOutputStreamWriter = new BufferedWriter(socketOutputWriter);// buffered write the socket writer for more effeciency
+            
+            
+            // --------------------
+           
+            String message = "I am a client";
+            
+            // Send data to the server by buffered writing to the output stream
+            bufferedOutputStreamWriter.write(message); // write a msg to get it streamed out to the server
+            bufferedOutputStreamWriter.newLine();
+            
+            bufferedOutputStreamWriter.write("I am quitting");
+            bufferedOutputStreamWriter.flush(); // flush/clear data
+            
+            String responseData = null; // the data that comes from the server as a response
+            
+           // Read data comming from the server
+            while ( bufferedInputStreamReader.readLine() != null ){
+                
+                responseData += bufferedInputStreamReader.readLine();
+                System.out.println("***** Server :"+ responseData);
+                
+                if(responseData.contains("7Tango") ){// if the server responds with '7Tango' the communication ends
+                    break;
+                }
+            }
+            
+            // close communication (Input and Output)
+            bufferedOutputStreamWriter.close();
+            bufferedInputStreamReader.close();
+            
         } catch (IOException ex) {
             Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
         }
+        
 
     }
 }
--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -14,11 +14,12 @@ public class Server {
         int port = ServConfig.PORT;
         
         ServerSocket server = new ServerSocket(port);
+        System.out.println("-------\n Server is listening on port 3008 \n-------");
         
         clientSocket = server.accept();
         
         if(clientSocket != null){
-            System.out.println("Connection successfully established with: "+clientSocket.getInetAddress());
+            System.out.println("\n +++++++++++++++++++++ \n Connection successfully established with: "+clientSocket.getInetAddress() +"\n +++++++++++++++++++++ \n");
         }
         
         
